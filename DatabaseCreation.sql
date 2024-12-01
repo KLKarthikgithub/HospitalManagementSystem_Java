009798144fb6efc538f2fcaafbfdfd6ce5f80c99
KLKarthikgithub <149095648+KLKarthikgithub@users.noreply.github.com>
2024-12-01 08:23:06 +0530

    Update DatabaseCreation.sql

--- DatabaseCreation.sql
+++ DatabaseCreation.sql
@@ -1,18 +1,19 @@
--- for creating a new database for patient reords in mysql workbench
-CREATE DATABASE PatientManagementDB;
+-- Create the database
+CREATE DATABASE hospital;
 
--- for use of database PatientManagementDB
-USE PatientManagementDB;
+-- Use the hospital database
+USE hospital;
 
-
---later
-
--- for creating a table in PatientManagementDB Database 
+-- Create the patients table
 CREATE TABLE patients (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     age INT NOT NULL,
     gender ENUM('Male', 'Female', 'Other') NOT NULL,
-    contact_number VARCHAR(15),
-    address TEXT
+    contact VARCHAR(15) NOT NULL,
+    date_of_joining DATETIME NOT NULL, -- Changed to DATETIME to include time
+    status VARCHAR(50) NOT NULL -- Changed from ENUM to VARCHAR(50)
 );
+
+-- If you need to alter the status column after creating the table, you can use:
+-- ALTER TABLE patients MODIFY status VARCHAR(50);
